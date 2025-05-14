342b330aeb689ccab0d03515dd91fa6a1df6a655
Himal Gunawardhana <gunawar-ec20005@stu.kln.ac.lk>
2025-05-14 06:23:02 +0000

    structure

--- src/Ch02/02_09b/CodeDemo.cpp
+++ src/Ch02/02_09b/CodeDemo.cpp
@@ -7,8 +7,19 @@
 
 enum class character_role {protagonist, antagonist, sidekick, npc};
 
+struct game_character{
+    std::string name;
+    int level;
+    character_role role;
+};
+
 int main(){
+    game_character buddy;
+    buddy.name = "Himal";
+    buddy.level = 50;
+    buddy.role = character_role::npc;
     
+    std::cout << buddy.name << " is a level " << buddy.level << " character with role of " << (int) buddy.role << std::endl;
     std::cout << std::endl << std::endl;
     return 0;
 }
